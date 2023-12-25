2e3b862befed134afc401eac8a9ed217d0e36c90
Jetar Pajaziti <jp58270@ubt-uni.net>
2023-12-25 23:14:45 +0100

    i just added routes and crated a nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import './pages/nav.jsx'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 function App() {
   return (
-   <div>
-    <h1>test</h1>
-    <nav />
-   </div>
+    <Box width="400px">
+      Navbar
+      <Routes>
+        <Route path="/" element ={<Home />}/>
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 }
 
--- src/pages/nav.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const nav = () => {
-  return (
-    <p>This is a nav bar</p>
-  )
-}
